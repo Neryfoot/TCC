1844b594cc2d7aebed027305674fac313b3adfba
Nery <nery.marques@hotmail.com>
2020-09-24 21:35:29 -0300

    update tanksim

--- TankSimulationCom.py
+++ TankSimulationCom.py
@@ -3,7 +3,7 @@ from multiprocessing import Process, Value
 import serial
 
 h = 0.1  # passo da solução numérica
-Fin = Value('f', 2.5)  # fluxo de entrada
+Fin = Value('f', 4.0)  # fluxo de entrada
 yk_1 = 0  # condição inicial
 yk_sync = Value('f', 0)  # condição inicial
 
@@ -20,19 +20,34 @@ def read_data(ser):  # read bytes until read \r(carriage return)
             buffer += one_byte.decode()
 
 
-def device02(ser):  # simulate device behavior
+def communication(ser):  # simulate device behavior
     buffer = b""
     while True:
         one_byte = ser.read(1)
         if one_byte == b"\r":
             if buffer[0:3] == b"#02":
-                data = b">" + bytes(str(Fin.value), "ascii") + b"\r"
-                ser.write(data)
-            else:
-                data = b"?"
-                ser.write(data)
+                device02(buffer, ser)
+                buffer = b""
+            if buffer[0:3] == b"#03":
+                device03(buffer, ser)
+                buffer = b""
         else:
             buffer += one_byte
+
+
+def device02(buffer, ser):  # simulate device behavior
+    data = b">" + bytes(str(yk_sync.value), "ascii") + b"\r"
+    ser.write(data)
+    print('02')
+
+
+def device03(buffer, ser):  # simulate device behavior
+    data = buffer
+    data = data[4:]
+    data = data[:-1]
+    data = float(data)
+    Fin.value = data
+    print('03')
 
 
 def dynamic():
@@ -56,9 +71,10 @@ def dynamic():
                 yk_1 = yk
                 steps -= 1
         yk_sync.value = yk
+        print(yk)
 
 
 dynamic_process = Process(target=dynamic)
-device02_process = Process(target=device02, args=(ser,))
+com_process = Process(target=communication, args=(ser,))
 dynamic_process.start()
-device02_process.start()
+com_process .start()
